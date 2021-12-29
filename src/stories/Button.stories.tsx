5d37adf9e61046dcbe0e17af5d195d5f26dcf098
ylyu <ylyu@Heyi.com>
2021-12-29 14:00:08 +0800

    test deploy 0

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@
  * @Author: ylyu
  * @Date: 2021-12-22 14:18:32
  * @LastEditors: ylyu
- * @LastEditTime: 2021-12-29 13:51:38
+ * @LastEditTime: 2021-12-29 14:00:00
  * @Description: Button
  */
 import { ComponentStory, ComponentMeta } from '@storybook/react';
@@ -34,7 +34,7 @@ export const Primary: ComponentStory<typeof Button> = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   type: 'primary',
-  label: 'Button3',
+  label: 'Button0',
 };
 
 // export const Sample = () => ({
