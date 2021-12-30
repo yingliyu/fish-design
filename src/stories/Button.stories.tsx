6db1d2365dcd81a63d185ad8b2b782fbcf88daae
ylyu <ylyu@Heyi.com>
2021-12-30 17:11:35 +0800

    test sync gitee gh-pages

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@
  * @Author: ylyu
  * @Date: 2021-12-22 14:18:32
  * @LastEditors: ylyu
- * @LastEditTime: 2021-12-30 09:58:15
+ * @LastEditTime: 2021-12-30 17:10:54
  * @Description: Button
  */
 import { ComponentStory, ComponentMeta } from '@storybook/react';
@@ -34,7 +34,7 @@ export const Primary: ComponentStory<typeof Button> = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   type: 'primary',
-  label: 'Button 1',
+  label: '按钮',
 };
 
 // export const Sample = () => ({
